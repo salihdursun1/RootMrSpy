ef7681ea913a86a80bb648ff36a3c92fd0d88f68
RootMrSpy <88969058+salihdursun1@users.noreply.github.com>
2021-08-22 10:27:47 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<link href="https://fonts.googleapis.com/css?family=Iceland" rel="stylesheet" type="text/css">
+<embed src="https://www.youtube.com/v/ChMkD1MdlWc&autoplay=1" type="application/x-shockwave-flash" wmode=transparent width=0 height=0> 
+<body bgcolor=black> </body>
+<center>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<title> Hacked by ./MarioGanz </title>
+	<font size="12" face="Iceland" color=red><b>Hacked by RootMrSpy </b></font>
+	<br>
+	<font size="6" face="Iceland" color=red><b>team<font size="6" face="Iceland" color=white>Root</b></font>
+	<center>
+		<font size="6" face="Iceland" color=red><b>we are as close<font size="6" face="Iceland" color=white>as a breath</b></font>
+	<br>
+	<br>
+	
+</center>
+</html>
